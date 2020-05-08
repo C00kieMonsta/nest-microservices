5d696923ed44cc3c512263fde4823de94e5fac39
Antoine Boxho <antoineboxho@Antoines-MacBook-Pro.local>
2020-05-09 01:08:18 +0200

    setup api gateway and services

---
+++ api-gateway/src/app.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get } from '@nestjs/common';
+import { zip } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/ping-a')
+  pingServiceA() {
+    return this.appService.pingServiceA();
+  }
+
+  @Get('/ping-b')
+  pingServiceB() {
+    return this.appService.pingServiceB();
+  }
+
+  @Get('/ping-c')
+  pingServiceC() {
+    return this.appService.pingServiceC();
+  }
+
+  // composing requests
+  @Get('/ping-all')
+  pingAll() {
+    return zip(
+      this.appService.pingServiceA(),
+      this.appService.pingServiceB(),
+      this.appService.pingServiceC()
+    ).pipe(
+      map(([dataA, dataB, dataC]) => ({
+        dataA,
+        dataB,
+        dataC
+      }))
+    );
+  }
+
+}
